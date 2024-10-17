1a2f707857616ada6b747538a1fd610c8be7c4dc
Anthony Gorski <anthony.developpeurweb@gmail.com>
2024-10-17 16:09:57 +0200

    ✨ (index.html, script.js, styles.css): Add dark theme functionality to the todo list application for better user experience and customization. Add buttons to toggle between different filters (All, Active, Completed) and clear all completed todos.

--- script.js
+++ script.js
@@ -3,7 +3,28 @@ document.addEventListener("DOMContentLoaded", () => {
 	const nbTodos = document.querySelector(".items-left");
 	const todoInput = document.querySelector("#new-todo-input");
 	const todoList = document.querySelector(".todo-list");
+	const filter_all = document.querySelector(".filter-all");
+	const filter_active = document.querySelector(".filter-active");
+	const filter_completed = document.querySelector(".filter-completed");
+	const clearCompleted = document.querySelector(".clear-completed");
+	const theme = document.querySelector("#theme");
 
+	function resetActiveFilter() {
+		filter_all.classList.remove("active");
+		filter_active.classList.remove("active");
+		filter_completed.classList.remove("active");
+	}
+
+	theme.addEventListener("click", () => {
+		document.body.classList.toggle("dark-theme");
+		if (document.body.classList.contains("dark-theme")) {
+			document.querySelector("#icon").src = "./images/icon-sun.svg";
+		} else {
+			document.querySelector("#icon").src = "./images/icon-moon.svg";
+		}
+	});
+
+	// Add new todo
 	todoInput.addEventListener("keypress", (e) => {
 		if (e.key === "Enter" && todoInput.value.trim() !== "") {
 			const newTodo = document.createElement("li");
@@ -29,10 +50,53 @@ document.addEventListener("DOMContentLoaded", () => {
 		}
 	});
 
+	// Delete all todos
+	clearCompleted.addEventListener("click", () => {
+		Array.from(todoItems).forEach((todo) => {
+			todo.remove();
+			nbTodos.textContent = "No items left";
+		});
+	});
+
+	// Display only completed todos
+	filter_active.addEventListener("click", () => {
+		Array.from(todoItems).forEach((todo) => {
+			if (todo.querySelector(".todo-checkbox").checked) {
+				todo.style.display = "none";
+			} else {
+				todo.style.display = "flex";
+			}
+		});
+		resetActiveFilter();
+		filter_active.classList.add("active");
+	});
+
+	// Display only active todos
+	filter_completed.addEventListener("click", () => {
+		Array.from(todoItems).forEach((todo) => {
+			if (todo.querySelector(".todo-checkbox").checked) {
+				todo.style.display = "flex";
+			} else {
+				todo.style.display = "none";
+			}
+		});
+		resetActiveFilter();
+		filter_completed.classList.add("active");
+	});
+
+	// Display all todos
+	filter_all.addEventListener("click", () => {
+		Array.from(todoItems).forEach((todo) => {
+			todo.style.display = "flex";
+		});
+		resetActiveFilter();
+		filter_all.classList.add("active");
+	});
+
+	// Remove todo
 	todoList.addEventListener("click", (e) => {
 		if (e.target.parentElement.classList.contains("delete-btn")) {
 			e.target.parentElement.parentElement.remove();
-
 			if (todoItems.length === 1) {
 				nbTodos.textContent = "1 item left";
 			} else if (todoItems.length === 0) {
